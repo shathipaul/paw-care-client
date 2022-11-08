f484390ea4740e705c5a5938da83ab1c1c26f6ae
shathipaul <shathipaul11@gmail.com>
2022-11-08 17:02:51 +0600

    added register

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -6,34 +6,35 @@ const Login = () => {
     return (
         <div className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
-            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
-                    <h1 className="text-5xl font-bold">Login please!</h1>
+                        <h1 className="text-5xl font-bold">Login please!</h1>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="label-text font-medium text-lg">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Password</span>
+                                <span className="label-text font-medium text-lg">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                             <label className="label">
-                                {/* <a href="#" >Forgot password?</a> */}
-                                <p>Don't have a account? <Link className='text-blue-900 font-medium' to='/register'>Register</Link></p>
+                                <p>Don't have a account?
+                                    <Link className='text-blue-900 font-medium' to='/register'>Register</Link>
+                                </p>
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <button className="btn btn-primary font-bold text-lg">Login</button>
                         </div>
                     </div>
                 </div>
                 <div >
                     <img src={img} alt="" />
                 </div>
-               
+
             </div>
         </div>
     );
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import img from '../../Assets/images/login.jpg'
 
 const Register = () => {
@@ -10,24 +11,38 @@ const Register = () => {
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
-                    <h1 className="text-5xl font-bold">Register now!</h1>
+                        <h1 className="text-5xl font-bold">Register now!</h1>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="label-text font-medium text-lg">Name</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" name='name' placeholder="name" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Password</span>
+                                <span className="label-text font-medium text-lg">PhotoURL</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input type="text" name='photoUrl' placeholder="photoUrl" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
                             <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                <span className="label-text font-medium text-lg">Email</span>
+                            </label>
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" required/>
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text font-medium text-lg">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <p>Already have an account?
+                                    <Link className='text-blue-900 font-medium' to='/login'>Login</Link>
+                                </p>
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <button className="btn btn-primary font-bold text-lg">Register</button>
                         </div>
                     </div>
                 </div>
