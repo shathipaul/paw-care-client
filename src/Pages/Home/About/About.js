1f2fa1d8849912f734590935bce15ce22719f93e
shathipaul <shathipaul11@gmail.com>
2022-11-08 14:51:08 +0600

    added about

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import img from '../../../Assets/images/dog-care.png'
+import video from '../../../Assets/videos/video1.mp4'
 
 const About = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
+        <div className="hero bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={img} alt='' className="max-w-sm rounded-lg shadow-2xl" />
-                <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                <video src={video} controls autoPlay={true} className="w-2/5"></video>
+                <div className='w-1/2 p-6'>
+                    <h1 className="text-5xl font-bold">What's Special About Paw Care!</h1>
+                    <p className="py-6 text-xl font-normal">We offer quick & easy services for both dogs and cat of various breeds. No matter their
+                        size or age, we can provide positive grooming experience.</p>
+                    <p className="py-3 text-xl font-normal">We have an experienced qualified
+                        team to take care of your best
+                        friend.</p>
                 </div>
             </div>
         </div>
